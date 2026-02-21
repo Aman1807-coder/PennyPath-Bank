7647514cea263d9b1d5605001b511594b33ac065
AMAN <amansrivastav1825@gmail.com>
2026-02-22 02:17:48 +0530

    Database setup

---
+++ src/test/java/com/example/pennypath/PennypathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pennypath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PennypathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
